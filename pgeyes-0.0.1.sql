3e9d21b199c788e009c177f20ad94fc22044d0b9
Yung-Chung Ku <ycku@csie.org>
2018-02-21 15:03:32 +0800

    Update pgeyes-0.0.1.sql

--- pgeyes-0.0.1.sql
+++ pgeyes-0.0.1.sql
@@ -1,6 +1,6 @@
 -- complain if script is sourced in psql, rather than via CREATE EXTENSION
 \echo Use "CREATE EXTENSION pg-eyes" to load this file. \quit
-CREATE FUNCTION pgeyes()
+CREATE OR REPLACE FUNCTION pgeyes()
 RETURNS boolean
 LANGUAGE plpgsql IMMUTABLE STRICT
   AS $$
