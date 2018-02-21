d6a31e571562292352d49014d8d9b73944681952
Yung-Chung Ku <ycku@csie.org>
2018-02-21 15:54:09 +0800

    Update pgeyes--0.0.1.sql

--- pgeyes--0.0.1.sql
+++ pgeyes--0.0.1.sql
@@ -1,10 +1,25 @@
 -- complain if script is sourced in psql, rather than via CREATE EXTENSION
 \echo Use "CREATE EXTENSION pgeyes" to load this file. \quit
-CREATE OR REPLACE FUNCTION pgeyes()
-RETURNS boolean
-LANGUAGE plpgsql IMMUTABLE STRICT
-  AS $$
-    BEGIN
-    RETURN true;
-    END;
-  $$;
+CREATE OR REPLACE FUNCTION pgeyes() 
+ RETURNS TABLE (
+ item VARCHAR,
+ result VARCHAR,
+ valid BOOLEAN,
+ description VARCHAR
+) 
+AS $$
+BEGIN
+	item := 'PostgreSQL version number';
+	result := current_setting('server_version_num');
+	IF cast(result as INTEGER) >= 90600 THEN
+		valid := true;
+	ELSE 
+		valid := false;
+	END IF;
+	description := '>=90600';
+
+        RETURN NEXT;
+           
+END; $$ 
+ 
+LANGUAGE 'plpgsql';
